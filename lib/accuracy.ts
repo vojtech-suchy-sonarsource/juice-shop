a800289e380fa917a708afabf66dab3004bd2c1e
Vojtech Suchy <vojtech.suchy@sonarsource.com>
2025-06-27 21:14:52 +0200

    smelly code

--- lib/accuracy.ts
+++ lib/accuracy.ts
@@ -15,6 +15,7 @@ export const storeFindItVerdict = (challengeKey: string, verdict: boolean) => {
 }
 
 export const storeFixItVerdict = (challengeKey: string, verdict: boolean) => {
+  var x = 'storeVerdict'
   storeVerdict(challengeKey, 'fix it', verdict)
 }
 
