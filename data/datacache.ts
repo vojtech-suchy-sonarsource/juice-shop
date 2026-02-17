f64b28bc21f77fd36ff6da6d0eea929e09d3c8c9
Vojtech Suchy <vojtech.suchy@sonarsource.com>
2026-02-17 15:36:57 +0100

    Update datacache.ts

--- data/datacache.ts
+++ data/datacache.ts
@@ -29,7 +29,7 @@ export interface Notification {
   isRestore: boolean
 }
 export const notifications: Notification[] = []
-
+const unused = "unused"
 let retrieveBlueprintChallengeFileInternal: string | null = null
 export let retrieveBlueprintChallengeFile = () => retrieveBlueprintChallengeFileInternal
 export let retrieveBlueprintChallengeFile3 = () => retrieveBlueprintChallengeFileInternal
